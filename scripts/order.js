10f18cdd1c1d2bf6720602886b4c751c7521c1d2
Ariel Cardoso <arielvix@gmail.com>
2020-09-16 16:00:25 -0700

    Load products from json

---
+++ scripts/order.js
@@ -0,0 +1,41 @@
+let products = [];
+let cartItems = [];
+let finalAmount = 0;
+
+$(document).ready(function () {
+  loadProducts();
+});
+
+function loadProducts() {
+  $.getJSON("products.json", function(data) {
+    $.each(data, function (key, item) {
+      products.push({
+        "id": item.id,
+        "group": item.group,
+        "name": item.name,
+        "description": item.description,
+        "image": item.image,
+        "price": item.price,
+        "isnew": (item.new)? true : false});
+    });
+    createHtmlList()
+  });
+}
+
+function createHtmlList() {
+    for (i=0;i<products.length;i++) {
+        let html = (
+            '<div class="item">' +
+              '<img class="mr-3" src="' + products[i].image + '" alt="' + products[i].name + '">' +
+              '<div>' +
+                '<h5>' + products[i].name + ' - $'+ products[i].price +'</h5>' +
+                '<p>' + products[i].description + '</p>' +
+              '</div>' +
+              '<a class="ml-auto" id="btn_add" data-id="' + products[i].id + '" onclick="productAdded(this)" href="#purchase" >' +
+                  '<i class="fas fa-plus-circle text-success"></i>' +
+              '</a>' + ((products[i].isnews == true)? '<span class="badge badge-warning">NEW</span>' : '') +
+            '</div>');
+          $("#"+ products[i].group +"-list").append(html);
+    }
+
+}
