64a50b3c81350c99ed5c0d6873b25c2f57703ed4
Ariel Cardoso <arielvix@gmail.com>
2020-09-18 18:32:44 -0700

    Add and remove items

--- scripts/order.js
+++ scripts/order.js
@@ -31,7 +31,7 @@ function createHtmlList() {
                 '<h5>' + products[i].name + ' - ' + dollarFormat(products[i].price) + '</h5>' +
                 '<p>' + products[i].description + '</p>' +
               '</div>' +
-              '<a class="ml-auto" id="btn_add" data-id="' + products[i].id + '" onclick="productAdded(this)" href="#purchase" >' +
+              '<a class="ml-auto add-button" data-id="' + products[i].id + '" onclick="addItem(' + products[i].id + ')" href="#" >' +
                   '<i class="fas fa-plus-circle text-success"></i>' +
               '</a>' + ((products[i].isnews == true)? '<span class="badge badge-warning">NEW</span>' : '') +
             '</div>');
@@ -45,3 +45,51 @@ function dollarFormat(value) {
     let newValue = "$" + value.substr(0, pos) + "." + value.substr(pos)
     return newValue;
 }
+
+function addItem(productId) {
+  let currentItemQtd = cartItems[productId]? cartItems[productId] : 0;
+  cartItems[productId] = currentItemQtd+1;
+  updatedCartItems();
+  event.preventDefault();
+}
+
+function removeItem(productId) {
+  let currentItemQtd = cartItems[productId]? cartItems[productId] : 0;
+  cartItems[productId] = currentItemQtd -1;
+  updatedCartItems();
+  event.preventDefault();
+}
+
+function updatedCartItems() {
+  let html = '';
+  let totalItems = 0;
+  
+  for (let i=0; i < cartItems.length; i++) {
+    if (cartItems[i]) {
+      let product = products.find(item => item.id == i);
+      totalItems += cartItems[i];
+      html += (`
+        <tr>
+          <td>`+ product.name +` - `+ dollarFormat(product.price) +`</td>
+          <td class="text-right">`+ cartItems[i] +`</td>
+          <td class="text-center">
+            <a class="bt-remove" onclick="removeItem(`+ product.id +`)" href="#" >
+              <i class="fas fa-times-circle text-danger"></i>
+            </a>
+          </td>
+        </tr>`);
+    }
+  }
+
+  if (totalItems > 0) {
+    $("#cartCount").html(totalItems);
+    $("#btnOrderNow").removeClass("disabled");
+  } else {
+    $("#cartCount").html("0");
+    $("#btnOrderNow").addClass("disabled");
+    html = `<tr><td colspan="3" class="text-center" height="150"><br><br>No items added yet.</td></tr>`; 
+  }
+
+  $("#cart-table tbody").html(html);
+}
+
