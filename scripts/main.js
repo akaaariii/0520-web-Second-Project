0bafe6f8b15c465883912e0d089080a86cebf8c7
akaaariii <caaari23@gmail.com>
2020-10-09 15:51:42 -0700

    Sign in using cookie

--- scripts/main.js
+++ scripts/main.js
@@ -14,15 +14,15 @@ const stringSignInModal = (`
                 <div class="modal-body">
                 <form>
                     <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" >
+                        <label for="userEmail">Email address</label>
+                        <input type="email" class="form-control" id="userEmail" >
                     </div>
                     <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label for="userPassword">Password</label>
+                        <input type="password" class="form-control" id="userPassword">
                     </div>
                     <div class="form-group text-center">
-                        <button type="button" class="btn btn-block btn-success">CONTINUE</button>
+                        <button type="button" onclick="login()" class="btn btn-block btn-success">CONTINUE</button>
                     </div>
                 </form>
                 <hr>
@@ -83,9 +83,89 @@ const stringSignUpModal = (`
 </div>`);
 body.appendChild(createElementFromHTML(stringSignUpModal));
 
+
+function checkUserLogged(){
+    // check if there is some email registered in the cookie
+    let cookieInfo = document.cookie;
+    let dropdownMenu = document.querySelector('#checkSignin');
+    if(cookieInfo !== null){
+        let emailCookie = cookieInfo.split('=')[1];
+        let username = emailCookie.split('@')[0].toUpperCase();
+        // show the dropdown button
+        dropdownMenu.innerHTML = (`
+            <div class="dropdown">
+                <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user-alt"></i><span> MY ACCOUNT</span>
+                </button>
+                <div class="dropdown-menu pb-0" aria-labelledby="dropdownMenuButton">
+                    <p class="mb-2" style="text-align: center;">Welcome ${username}</p>
+                    <a class="dropdown-item" style="text-align: center; type="button" onclick="logout()" href="#">SIGN OUT</a>
+                 </div>
+            </div>   
+        `);
+    }
+}
+
+function logout(emailCookie){
+    // 1. delete email registered in the cookie
+    document.cookie = `user_email=${emailCookie}; expires=${new Date(2020,1,1).toUTCString()}`;
+
+    // 2. reload the page
+    console.log(document.cookie);
+    if(document.cookie === ''){
+        location.reload();
+    }
+    
+}
+
+
+function login(){
+    let email = $('#userEmail').val();
+    // let password = $('#userPassword').val();
+    let d = new Date();
+    let minutes = 30;
+    d.setTime(d.getTime() + (minutes * 60 * 1000));
+
+    // 2. register the cookie with user email
+    document.cookie = `user_email=${email}; expires=${d.toUTCString()}`;
+    
+    // 3. close the modal
+    $('#modalLogIn').modal('hide');
+    checkUserLogged();
+}
+
+
 function createElementFromHTML(htmlString) {
     let div = document.createElement('div');
     div.innerHTML = htmlString.trim();
     // Change this to div.childNodes to support multiple top-level nodes
     return div.firstChild; 
 }
+
+
+    // 1. check validation
+    // const ERROR_MESSAGE = {
+    //     requiredEmail : 'email is required',
+    //     validEmail : 'email is not valid',
+    //     requiredPassword : 'password is required'
+    // }
+    // const emailformat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // const small = document.createElement('small');
+    // small.classList.add('text-danger');
+    // if(email === ''){
+    //     small.textContent = ERROR_MESSAGE.requiredEmail;
+    // } else if (!emailformat.test(email)){
+    //     small.textContent = ERROR_MESSAGE.validEmail;
+    // } else {
+    //     small.classList.remove('text-danger');
+    //     small.classList.add('text-success');
+    //     small.textContent = "ok";
+    // }
+
+    // if(password === ''){
+    //     small.textContent = ERROR_MESSAGE.requiredPassword;
+    // } else {
+    //     small.classList.remove('text-danger');
+    //     small.classList.add('text-success');
+    //     small.textContent = "ok";
+    // }
